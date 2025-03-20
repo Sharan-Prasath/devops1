9aa1689393c0afc5bef0e2cadc060174ee6a36f9
Sharan-Prasath <125854898+Sharan-Prasath@users.noreply.github.com>
2025-03-20 15:17:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u sharanprasath -p dckr_pat_ptnaJaPC7LrPH-jRtOIDg5hsYek
+    docker tag test1 sharanprasath/dev
+    docker push sharanprasath/dev
+    #docker-compose up -d
     
