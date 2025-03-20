2b6a1eb39989b02675a8e1f4aa27defd25c81317
Sharan-Prasath <125854898+Sharan-Prasath@users.noreply.github.com>
2025-03-20 15:49:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u sharanprasath -p dckr_pat_ptnaJaPC7LrPH-jRtOIDg5hsYek
-    docker tag test1 sharanprasath/test1
-    docker push sharanprasath/test1
+    docker login -u sharanprasath -p dckr_pat_bRQZdNyGkPJCA_cXHOC0rw6Kcx4
+    docker tag test2 sharanprasath/test2
+    docker push sharanprasath/test2
 
